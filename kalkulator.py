787f259dc46f5b3482cbf7cab01831af76fdd5d5
Pio0001 <student@lab-kie.(none)>
2014-05-14 10:52:16 +0200

    nowa funkcja dodawania dziala

--- kalkulator.py
+++ kalkulator.py
@@ -1,6 +1,5 @@
-def dodawanie():
-    a = int(input('Wprowadz pierwsza liczbe'))
-    b = int(input('Wprowadz druga liczbe'))
-    print (a+b)
-
-dodawanie()
+def dodawanie(a,b):
+    return a+b
+l1 = int(input('Wprowadz pierwsza liczbe'))
+l2 = int(input('Wprowadz druga liczbe'))
+print(dodawanie(l1,l2))
